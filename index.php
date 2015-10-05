b62c25725f1b2c924ef6d057bceeecc6c85580b1
bahumbert <humbertb.info@gmail.com>
2015-10-05 19:28:11 +0200

    cookie

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-﻿<?php session_start();?>
+﻿<?php session_start();
+include("./languages/manage_languages.php");
+?>
 
 <!DOCTYPE html>
 	<head>
@@ -12,24 +14,24 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-6 col-md-4 col-md-offset-4">
-					<h1 class="text-center login-title">ZZTasks : <?php //echo $TXT_;?>
+					<h1 class="text-center login-title">ZZTasks : <?php echo $TXT_TITLE;?>
 					</h1>
 					<div class="account-wall">
 						<img class="profile-img" src="http://thesocialmediamonthly.com/wp-content/uploads/2015/08/photo.png"
 						 alt="">
 						<form class="form-signin" action="login.php" method="POST">
-						<input type="text" class="form-control" placeholder="Username" name="usr" required autofocus value="<?php if(isset($_COOKIE['login'])){ echo $_COOKIE['login'];}  ?>" >
-						<input type="password" class="form-control" placeholder="Password" name="pwd" required>
+						<input type="text" class="form-control" placeholder="<?php echo $TXT_USERNAME;?>" name="usr" required autofocus value="<?php if(isset($_COOKIE['login'])){ echo $_COOKIE['login'];}  ?>" >
+						<input type="password" class="form-control" placeholder="<?php echo $TXT_PASSWORD;?>" name="pwd" required>
 						<button class="btn btn-lg btn-primary btn-block" type="submit">
-							Sign in</button>
+							<?php echo $TXT_SIGN;?></button>
 						<label class="checkbox pull-left">
 							<input type="checkbox" value="remember-me">
-							Remember me
+							<?php echo $TXT_REMEMBER;?>
 						</label>
-						<a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+						<a href="#" class="pull-right need-help"><?php echo $TXT_HELP;?> </a><span class="clearfix"></span>
 						</form>
 					</div>
-					<a href="#" class="text-center new-account">Create an account </a>
+					<a href="#" class="text-center new-account"><?php echo $TXT_CREATE;?> </a>
 				</div>
 			</div>
 		</div>
--- languages/index_fr.php
+++ languages/index_fr.php
@@ -5,7 +5,13 @@ Index Language: French
 ------------------
 */
 
-
+$TXT_TITLE = "Connexion";
+$TXT_USERNAME = "Entrez un nom d'utilisateur";
+$TXT_PASSWORD = "Entrez un mot de passe";
+$TXT_SIGN = "Valider";
+$TXT_REMEMBER = "Se souvenir de moi";
+$TXT_HELP = "Besoin d'aide";
+$TXT_CREATE = "Créer un compte";
 
 
 
